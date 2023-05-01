f586856c6cf03057c360e80ca88cdb8fa37d2d76
HyojongJeon <blackcubes@naver.com>
2023-05-01 19:57:04 +0900

    client-server communication

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-/* eslint-disable no-console, spaced-comment, func-call-spacing, no-spaced-func */
-
 //==============================================================
 // This is an example of polling (reading) Holding Registers
 // on a regular scan interval with timeouts enabled.
 // For robust behaviour, the next action is not activated
 // until the previous action is completed (callback served).
 //==============================================================
-
 "use strict";
-
 //==============================================================
+
+
+// Add require
+const express   = require('express');
+const ModbusRTU = require ("modbus-serial");
+
 // create an empty modbus client
-const ModbusRTU   = require ("modbus-serial");
-const client      = new ModbusRTU();
+const app     =  express();
+const client  =  new ModbusRTU();
+
+app.use(express.json()); // POST 요청에서 JSON 파일을 PARSING 하기 위해 필요
 
 let mbsStatus   = "Initializing...";    // holds a status of Modbus
 
@@ -26,39 +30,12 @@ const MBS_STATE_GOOD_CONNECT  = "State good (port)";
 const MBS_STATE_FAIL_CONNECT  = "State fail (port)";
 
 // Modbus configuration values
-const mbsId       = 1;
+
 const mbsScan     = 1000;
 const mbsTimeout  = 5000;
 let mbsState    = MBS_STATE_INIT;
 
-// Upon SerialPort error
-client.on("error", function(error) {
-    console.log("SerialPort Error: ", error);
-});
 
-
-//==============================================================
-const connectClient = function()
-{
-    // set requests parameters
-    client.setID      (mbsId);
-    client.setTimeout (mbsTimeout);
-
-    // try to connect
-    client.connectRTUBuffered ("COM6", { baudRate: 38400, parity: "none", dataBits: 8, stopBits: 1 })
-        .then(function()
-        {
-            mbsState  = MBS_STATE_GOOD_CONNECT;
-            mbsStatus = "Connected, wait for reading...";
-            console.log(mbsStatus);
-        })
-        .catch(function(e)
-        {
-            mbsState  = MBS_STATE_FAIL_CONNECT;
-            mbsStatus = e.message;
-            console.log(e);
-        });
-};
 
 
 //==============================================================
@@ -81,16 +58,15 @@ const readModbusData = function()
         });
 };
 
-function writeRegisters() {
+function writeRegisters()  {
     // write 3 registers statrting at register 101
     // negative values (< 0) have to add 65535 for Modbus registers
-    client.writeRegisters(0, [101])
+    client.writeRegisters(0, [101, 0])
         .then(function(d) {
             console.log("Write 101 0 0", d); })
         .catch(function(e) {
             console.log(e.message); })
-}
-
+};
 
 //==============================================================
 const runModbus = function()
@@ -128,7 +104,7 @@ const runModbus = function()
             // nothing to do, keep scanning until actionable case
     }
 
-    console.log();
+    // console.log();
     console.log(nextAction);
 
     // execute "next action" function if defined
@@ -142,6 +118,72 @@ const runModbus = function()
     setTimeout (runModbus, mbsScan);
 };
 
-//==============================================================
-runModbus();
-setTimeout (writeRegisters, 1500);
+//============Server-Client Data Exchange============//
+
+// Open Server //
+app.listen(3000, () => {
+    console.log('Server listening on "http://localhost:3000/"');
+});
+
+//============ MODBUS RTU Data Exchange =============//
+var comPort;
+var bitRate;
+var modbusID;
+
+/* 1. Upon SerialPort error */
+client.on("error", function(error) {
+    console.log("SerialPort Error: ", error);
+});
+
+/* 2. Open MODBUS RTU Serial Port from Client Reqeust*/
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+app.post('/api/connectClient', (req, res) => {
+    // 클라이언트에서 전송한 데이터 처리 코드
+    const data = req.body;
+
+    comPort = data.comPort;
+    bitRate = data.bitRate;
+    modbusID = data.modbusID;
+    console.log("PORT = "+comPort+", BITRATE = "+bitRate+", MODBUS ID = "+modbusID);
+    res.send('Client: connect MODBUS RTU Serial Port');
+    connectClient(Number(bitRate), comPort, Number(modbusID));
+});
+
+app.post('/api/disconnectClient', (req, res) => {
+    disconnectClient();
+});
+
+app.post('/api/gripperPosCtrl', (req, res) => {
+    let data = req.body;
+    let gripperPosCtrl = Number(data.gripperPosCtrl);
+    console.log(gripperPosCtrl);
+});
+
+const connectClient = function(baudRateVal, comPortVal, modbusID) {
+    // set requests parameters
+    client.setID      (modbusID);
+    client.setTimeout (mbsTimeout);
+
+    // try to connect
+    client.connectRTUBuffered (comPortVal, { baudRate: baudRateVal, parity: "none", dataBits: 8, stopBits: 1 })
+        .then(function()
+        {
+            console.log("[USB Connected, wait for reading]");
+        })
+        .catch(function(e)
+        {
+            console.log(e);
+        });
+};
+
+const disconnectClient = function() {
+    client.close( function() {console.log(comPort+' 장치와의 연결이 종료되었습니다.')})
+};
+
+// runModbus();
+// setTimeout (writeRegisters, 1500);
+
+
---
+++ text.js
@@ -0,0 +1,11 @@
+let a = 1;
+let b = 2;
+
+const abc = function() {
+    a = 2;
+    b = 3;
+}
+
+abcd = abc();
+
+console.log(a);
